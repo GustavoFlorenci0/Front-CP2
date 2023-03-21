bad08493f320c3fd8d1d51dc07be0c903766b0ac
GustavoFlorenci0 <florencio152@gmail.com>
2023-03-20 22:51:18 -0300

    arquivos editados em grupo 20/03

---
+++ javascript/validacao.js
@@ -0,0 +1,10 @@
+// buscando os itens a validar.
+
+let nomeSignup = document.getElementById("nomeSignup");
+let sobrenomeSignup = document.getElementById("sobrenomeSignup");
+let emailSignup = document.getElementById("emailSignup");
+let senhaSignup = document.getElementById("senhaSignup");
+let senhaRepetidaSignup = document.getElementById("senhaRepetidaSignup");
+let btnSignup = document.getElementById("btnSignup");
+
+// validando 
