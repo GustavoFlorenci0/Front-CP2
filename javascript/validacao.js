8acff1927abda41d6a5e9ec1199ad28b5dfb3e9b
GustavoFlorenci0 <florencio152@gmail.com>
2023-03-21 18:56:44 -0300

    começando a validacao signup

--- javascript/validacao.js
+++ javascript/validacao.js
@@ -7,4 +7,70 @@ let senhaSignup = document.getElementById("senhaSignup");
 let senhaRepetidaSignup = document.getElementById("senhaRepetidaSignup");
 let btnSignup = document.getElementById("btnSignup");
 
-// validando 
+let nomeSmall = document.getElementById("nomeSmall");
+let sobrenomeSmall = document.getElementById("sobrenomeSmall");
+let emailSmall = document.getElementById("emailSmall");
+let senhaSmall = document.getElementById("senhaSmall");
+let senhaRepetidaSmall = document.getElementById("senhaRepetidaSmall");
+// validando informações; 
+
+nomeSignup.addEventListener("keyup", function () {
+    if (nomeSignup.value.length >= 4) {
+        nomeSmall.innerText = "Ok!";
+        nomeSmall.style.color = "green";
+
+    } else {
+        nomeSignup.style.backgroundColor = "#ffffff";
+        nomeSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        nomeSmall.style.color = "red";
+    }
+})
+
+sobrenomeSignup.addEventListener("keyup", function () {
+    if (sobrenomeSignup.value.length >= 4) {
+        sobrenomeSmall.innerText = "Ok!";
+        sobrenomeSmall.style.color = "green";
+
+    } else {
+        sobrenomeSignup.style.backgroundColor = "#ffffff";
+        sobrenomeSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        sobrenomeSmall.style.color = "red";
+    }
+})
+
+emailSignup.addEventListener("keyup", function () {
+    if (emailSignup.value.length >= 4) {
+        emailSmall.innerText = "Ok!";
+        emailSmall.style.color = "green";
+
+    } else {
+        emailSignup.style.backgroundColor = "#ffffff";
+        emailSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        emailSmall.style.color = "red";
+    }
+})
+
+senhaSignup.addEventListener("keyup", function () {
+    if (senhaSignup.value.length >= 4) {
+        senhaSmall.innerText = "Ok!";
+        senhaSmall.style.color = "green";
+
+    } else {
+        senhaSignup.style.backgroundColor = "#ffffff";
+        senhaSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        senhaSmall.style.color = "red";
+    }
+})
+
+senhaRepetidaSignup.addEventListener("keyup", function () {
+    if (senhaRepetidaSignup.value.length >= 4) {
+        senhaRepetidaSmall.innerText = "Ok!";
+        senhaRepetidaSmall.style.color = "green";
+
+    } else {
+        senhaRepetidaSignup.style.backgroundColor = "#ffffff";
+        senhaRepetidaSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        senhaRepetidaSmall.style.color = "red";
+    }
+})
+
