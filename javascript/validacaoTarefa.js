b9263b81189bdd8f6ea3005cfeea36232024a659
felipe-2833 <69013950+felipe-2833@users.noreply.github.com>
2023-04-04 10:34:38 -0300

    obtendo nome do usuario

--- javascript/validacaoTarefa.js
+++ javascript/validacaoTarefa.js
@@ -12,3 +12,48 @@ novaTarefa.addEventListener("keyup", function () {
         
     }
 })
+
+let jwt;
+
+onload = function () {
+
+    jwt = sessionStorage.getItem("token");
+
+    obterNomeUsuario();
+}
+
+function obterNomeUsuario() {
+    let request = {
+        headers: {
+            'Authorization': jwt
+        }
+    }
+
+    fetch(`${baseUrlApi()}/users/getMe`, request)
+    .then(resultado => {
+
+        if (resultado.status == 201 || resultado.status == 200) {
+            return resultado.json();
+        } else {
+
+            throw resultado;
+        }
+    }
+    ).then(
+        resultado => {
+            NomeUsuario(resultado)
+        }
+        )
+        .catch(
+            erro => {
+                if (erro.status == 400 || erro.status == 404) {
+                    console.log("error");
+                }
+            }
+        );
+}
+
+function NomeUsuario(usuario) {
+    let userInfoP = document.getElementById("nomeUsuario");
+    userInfoP.innerText = `${usuario.firstName} ${usuario.lastName}`
+}
