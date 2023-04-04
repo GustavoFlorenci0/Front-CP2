f27768dc5f90d3c181d78d94a4f60225b2f13078
felipe-2833 <69013950+felipe-2833@users.noreply.github.com>
2023-04-03 23:40:57 -0300

    Criando comunicação com API

--- javascript/validacaoSingup.js
+++ javascript/validacaoSingup.js
@@ -22,6 +22,10 @@ let emailIsValid = false;
 let senhaIsValid = false;
 let senhaRepetidaIsValid = false;
 
+let usuarioJs = {};
+let usuarioJson = "";
+
+
 // adicionando um evento para cadastro
 
 btnSignup.addEventListener("click", async function (evento) {
@@ -43,22 +47,52 @@ btnSignup.addEventListener("click", async function (evento) {
         emailSignup = normalizaStringUsandoTrim(emailSignup.value);
         senhaRepetidaSignup = normalizaStringUsandoTrim(senhaRepetidaSignup.value);
 
-        let usuarioJs = {
-            name: nomeSignup,
+        usuarioJs = {
+            firstName: nomeSignup,
             lastName: sobrenomeSignup,
             email: emailSignup,
             password: senhaRepetidaSignup
         }
 
-        let usuarioJson = JSON.stringify(usuarioJs);
-        //loginApi(usuarioJson);
+        usuarioJson = JSON.stringify(usuarioJs);
+
+        SingupAPI(usuarioJson);
     } else {
         alert ("login invalid");
     }
-
-
-
 });
+
+function SingupAPI(UsuarioJson) {
+    let request = {
+        method: "POST",
+        body: UsuarioJson,
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }
+
+    fetch(`${baseUrlApi()}/users`, request)
+        .then(resultado => {
+
+            /* Verifica status de sucesso na execução da promisse */
+            if (resultado.status == 201 || resultado.status == 200) {
+                window.location.href = "index.html";
+                return resultado.json();
+            } else {
+                /* Caso o status não seja sucesso, retorna uma exceção com todo o objeto do "erro" */
+                throw resultado;
+            }
+        }
+        ).catch(
+            erro => {
+                if (erro.status == 400) {
+                    console.log("Usuario ja existe");
+                    alert("Usuário já existe");
+
+                }
+            }
+        );
+}
 
 // validando informações; 
 
@@ -79,6 +113,10 @@ function validaLogin() {
 }
 
 nomeSignup.addEventListener("keyup", function () {
+
+    nomeSignup = document.getElementById("nomeSignup");
+    nomeSmall = document.getElementById("nomeSmall");
+
     if (nomeSignup.value.length >= minNameCaracteres) {
         nomeSmall.innerText = "Ok!";
         nomeSmall.style.color = "green";
@@ -86,7 +124,7 @@ nomeSignup.addEventListener("keyup", function () {
 
     } else {
         nomeSignup.style.backgroundColor = "#ffffff";
-        nomeSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        nomeSmall.innerText = `Campo obrigatorio, faltam  ${minNameCaracteres - nomeSignup.value.length} caracteres`;
         nomeSmall.style.color = "red";
         nameIsValid = false;
     }
@@ -94,6 +132,10 @@ nomeSignup.addEventListener("keyup", function () {
 })
 
 sobrenomeSignup.addEventListener("keyup", function () {
+
+    sobrenomeSignup = document.getElementById("sobrenomeSignup");
+    sobrenomeSmall = document.getElementById("sobrenomeSmall");
+
     if (sobrenomeSignup.value.length >= minNameCaracteres) {
         sobrenomeSmall.innerText = "Ok!";
         sobrenomeSmall.style.color = "green";
@@ -101,7 +143,7 @@ sobrenomeSignup.addEventListener("keyup", function () {
 
     } else {
         sobrenomeSignup.style.backgroundColor = "#ffffff";
-        sobrenomeSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        sobrenomeSmall.innerText = `Campo obrigatorio, faltam  ${minNameCaracteres - sobrenomeSignup.value.length} caracteres`;
         sobrenomeSmall.style.color = "red";
         sobrenomeIsValid = false;
     }
@@ -109,9 +151,13 @@ sobrenomeSignup.addEventListener("keyup", function () {
 })
 
 emailSignup.addEventListener("keyup", function () {
+
+    emailSignup = document.getElementById("emailSignup");
+    emailSmall = document.getElementById("emailSmall");
+
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailSignup.value)) {
         emailSignup.style.backgroundColor = "#ffffff";
-        emailSmall.innerText = "Campo obrigatorio com 4 letras no minimo";
+        emailSmall.innerText = "Campo obrigatorio";
         emailSmall.style.color = "red";
         emailIsValid = false;
     } else {
@@ -123,6 +169,11 @@ emailSignup.addEventListener("keyup", function () {
 })
 
 senhaSignup.addEventListener("keyup", function () {
+
+    senhaSignup = document.getElementById("senhaSignup");
+    senhaSmall = document.getElementById("senhaSmall");
+
+
     if (senhaSignup.value.length >= minPasswordCaracteres) {
         senhaSmall.innerText = "Ok!";
         senhaSmall.style.color = "green";
@@ -130,7 +181,7 @@ senhaSignup.addEventListener("keyup", function () {
 
     } else {
         senhaSignup.style.backgroundColor = "#ffffff";
-        senhaSmall.innerText = "Campo obrigatorio com 5 letras no minimo";
+        senhaSmall.innerText = `Campo obrigatorio, faltam  ${minPasswordCaracteres - senhaSignup.value.length} caracteres`;
         senhaSmall.style.color = "red";
         senhaIsValid = false;
     }
@@ -138,6 +189,11 @@ senhaSignup.addEventListener("keyup", function () {
 })
 
 senhaRepetidaSignup.addEventListener("keyup", function () {
+
+    senhaSignup = document.getElementById("senhaSignup");
+    senhaRepetidaSignup = document.getElementById("senhaRepetidaSignup");
+    senhaRepetidaSmall = document.getElementById("senhaRepetidaSmall");
+
     if (senhaRepetidaSignup.value == senhaSignup.value && senhaIsValid == true) {
         senhaRepetidaSmall.innerText = "Ok!";
         senhaRepetidaSmall.style.color = "green";
